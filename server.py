da47e75db7bbc8d17911ffc060b90961d2e698ca
Hendrik.Kivi <hendrik.kivi@ttu.ee>
2018-09-29 16:26:58 +0300

    refactor

--- server.py
+++ server.py
@@ -2,15 +2,14 @@ from flask import Flask, render_template, request, jsonify
 import json
 from random import randint
 app = Flask(__name__, static_url_path='/static')
+questionsFile = "data/questions.json"
 
 @app.route("/")
 def index():
     page_name="index"
     version = randint(0,999999)
     question = getQuestion()
-    answer = 0
-    question = getFollowup(0)
-    print(question)
+    #question = getFollowup(0)
     answers = getAnswers()
     return render_template('%s.html' % page_name, question=question, answers=answers, version=version)
 
@@ -21,13 +20,13 @@ def getInjuries():
     return jsonify(fakeInjury)
 
 def getQuestion():
-    with open("data/questions.json", "r") as read_file:
-        data = json.load(read_file)
+    with open(questionsFile, "r") as readFile:
+        data = json.load(readFile)
         return(data['question'])
     
 def getFollowup(answer):
-    with open("data/questions.json", "r") as read_file:
-        data = json.load(read_file)
+    with open(questionsFile, "r") as readFile:
+        data = json.load(readFile)
         answer = data['answers'][answer]
         print(answer)
         if 'followup' in answer:
@@ -40,10 +39,9 @@ def getAnswers():
     return ["Yes","No"]
     
 @app.route('/getAnswer/', methods=['GET', 'POST'])
-def my_form_post():
-    qr = request.args.get('answ')
-    print(qr)
-    return ""
+def getAnswer():
+    answer = request.args.get('answ')
+    return answer
  
 if __name__ == "__main__":
     app.run()
